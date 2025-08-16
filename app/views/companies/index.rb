ab87e4efc670b70b055a53dac592286c0235a4e1
Walmir <owalmirneto@gmail.com>
2025-08-16 10:53:15 +0100

    Use components

--- app/views/companies/index.rb
+++ app/views/companies/index.rb
@@ -9,17 +9,18 @@ class Views::Companies::Index < Views::Base
       Components::LinkTo::New("New company", new_company_path)
     end
 
-    return render(Flashes::Warning.new(message: "Companies not found")) unless @companies.exists?
+    return render(Components::Flashes::Warning.new(message: "Companies not found")) unless @companies.exists?
 
     div(id: "companies") do
       Table do
-        # TableCaption { "Employees at Acme inc." }
+        TableCaption { "Companies" }
         TableHeader do
           TableRow do
             TableHead { "Name" }
             TableHead { "NIF" }
             TableHead { "Industry" }
             TableHead { "Disabled?" }
+            TableHead(class: "text-right") { "Role" }
           end
         end
 
@@ -30,15 +31,20 @@ class Views::Companies::Index < Views::Base
               TableCell { company.nif }
               TableCell { company.industry }
               TableCell { Components::Boolean::String(company.disabled?) }
+              TableCell(class: "text-right") do
+                Components::LinkTo::Edit(edit_company_path(company))
+                # Components::LinkTo::Destroy(company_path(company))
+              end
             end
           end
         end
-        TableFooter do
-          TableRow do
-            TableHead(class: "font-medium", colspan: 3) { "Total" }
-            TableHead(class: "font-medium text-right") { @companies.size }
-          end
-        end
+
+        # TableFooter do
+        #   TableRow do
+        #     TableHead(class: "font-medium", colspan: 3) { "Total" }
+        #     TableHead(class: "font-medium text-right") { @companies.size }
+        #   end
+        # end
       end
     end
   end
