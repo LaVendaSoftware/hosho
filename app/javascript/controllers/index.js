395e87e853f60fb4bc39fef567dfc462283cf1ba
Walmir <owalmirneto@gmail.com>
2025-08-17 11:26:01 +0100

    Add Form components

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,10 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
+
+import RubyUi__FormFieldController from "./ruby_ui/form_field_controller";
+application.register("ruby-ui--form-field", RubyUi__FormFieldController);
