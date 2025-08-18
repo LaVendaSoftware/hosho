d72fee46088e21c109912e2e8709dfef8abe1208
Walmir <owalmirneto@gmail.com>
2025-08-18 18:52:26 +0100

    Move input classes to css file

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -162,12 +162,7 @@ SimpleForm.setup do |config|
   # config.cache_discovery = !Rails.env.development?
 
   # Default class for inputs
-  config.input_class = [
-    "flex h-9 w-full rounded-md border bg-background px-3 py-1 text-sm shadow-sm ",
-    "transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium ",
-    "focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 ",
-    "border-border focus-visible:ring-ring placeholder:text-muted-foreground"
-  ].join
+  config.input_class = nil
 
   # Define the default class of the input wrapper of the boolean input.
   config.boolean_label_class = "checkbox"
