ce656b5a4bd26994f22a328ce85ac8e4c9ffb626
Walmir <owalmirneto@gmail.com>
2025-09-15 12:59:28 -0300

    Add helpers method to base components

--- app/components/base.rb
+++ app/components/base.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Components::Base < Phlex::HTML
+  include Components
   include RubyUI
   # Include any helpers you want to be available across all components
   include Phlex::Rails::Helpers::Routes
@@ -11,4 +12,11 @@ class Components::Base < Phlex::HTML
       super
     end
   end
+
+  private
+
+  def helpers
+    @helpers ||= ApplicationController.helpers
+  end
+  alias_method :h, :helpers
 end
