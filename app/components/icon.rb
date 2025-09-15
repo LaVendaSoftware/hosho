fea7f20bca1929f5db515ee4cc8c1515b9403f30
Walmir <owalmirneto@gmail.com>
2025-09-15 13:07:18 -0300

    Fix Icon component

--- app/components/icon.rb
+++ app/components/icon.rb
@@ -1,11 +1,12 @@
 class Components::Icon < Components::Base
-  def initialize(icon)
+  def initialize(icon, **options)
     @icon = icon
+    @options = options
   end
 
   def view_template
     return if @icon.blank?
 
-    i { ApplicationController.helpers.phosphor_icon(@icon) }
+    i { h.phosphor_icon(@icon, **@options) }
   end
 end
