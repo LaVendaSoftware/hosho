8392f383d47959d13835117e876558fa4ea4358c
Walmir <owalmirneto@gmail.com>
2025-09-14 23:25:17 -0300

    Fix shared context to simulate logged user

--- spec/support/shared_contexts/when_user_is_logged_in.rb
+++ spec/support/shared_contexts/when_user_is_logged_in.rb
@@ -1,12 +1,5 @@
 shared_context "when user is logged in" do
-  before do
-    visit(new_user_session_path)
-
-    fill_in(User.human_attribute_name(:email), with: current_user.email)
-    fill_in(User.human_attribute_name(:password), with: current_user.password)
-
-    click_on("Login")
-  end
+  before { login_as(current_user) }
 
   let(:current_user) { create(:user) }
 end
