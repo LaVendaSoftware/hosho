2998531092b3d90ca153279603ac850c6438d5e6
Walmir <owalmirneto@gmail.com>
2025-09-15 13:03:29 -0300

    Create page title components

--- app/components/page_title/base.rb
+++ app/components/page_title/base.rb
@@ -1,14 +1,24 @@
-class Components::PageTitle < Components::Base
-  def initialize(title, icon = nil)
+class Components::PageTitle::Base < Components::Base
+  include Phlex::Rails::Helpers::ContentFor
+
+  def initialize(title, icon: nil, link: nil, back_url: nil)
     @title = title
     @icon = icon
+    @link = link
+    @back_url = back_url
   end
 
   def view_template
+    content_for(:title, @title)
+
     header(class: header_classes) do
       div(class: "flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6") do
+        Components::LinkTo::Back(@back_url)
+
         Components::Icon(@icon)
         Heading(level: 1, size: 4) { @title }
+
+        div(class: "ml-auto") { Components::LinkTo::New(@link) }
       end
     end
   end
