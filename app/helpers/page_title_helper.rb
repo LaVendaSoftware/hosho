2249d3c13c114895f2553ade13de2a4c03d57024
Walmir <owalmirneto@gmail.com>
2025-09-15 00:08:58 -0300

    Fix layout application

--- app/helpers/page_title_helper.rb
+++ app/helpers/page_title_helper.rb
@@ -1,11 +1,12 @@
 module PageTitleHelper
+  def full_page_title
+    [page_title, app_name].compact.join(" » ")
+  end
+
   def page_title
-    [
-      content_for(:turbo_native_title).presence ||
-        content_for(:page_title).presence ||
-        content_for(:title).presence,
-      app_name
-    ].compact.join(" » ")
+    content_for(:turbo_native_title).presence ||
+      content_for(:page_title).presence ||
+      content_for(:title).presence
   end
 
   def platform_identifier
