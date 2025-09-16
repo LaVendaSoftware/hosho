738ca60e5ae8ce99a140f99d7fff59977c8f4a0f
Walmir <owalmirneto@gmail.com>
2025-09-16 09:29:21 -0300

    Create page component page show

---
+++ app/components/page/show_row.rb
@@ -0,0 +1,22 @@
+class Components::Page::ShowRow < Components::Base
+  def initialize(value:, label:, title: nil)
+    @value = value
+    @label = label
+    @title = title.presence || value
+  end
+
+  def view_template
+    div(
+      title: @title,
+      class: "flex gap-2 p-4 sm:gap-6 sm:flex-row sm:items-center"
+    ) do
+      p(
+        class:
+          "w-32 text-lg font-normal text-gray-500 sm:text-right dark:text-gray-400 shrink-0"
+      ) { @label }
+      h3(class: "text-lg font-semibold text-gray-900 dark:text-white") do
+        span { @value.presence || "—" }
+      end
+    end
+  end
+end
