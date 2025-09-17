b06c40c9f8fc674bcec092f7e5813d558b708902
Walmir <owalmirneto@gmail.com>
2025-09-17 19:10:29 -0300

    Create BelongsToField component

---
+++ app/components/form/belongs_to_field.rb
@@ -0,0 +1,12 @@
+class Components::Form::BelongsToField < Components::Base
+  def initialize(form, attribute, collection: [], **options)
+    @form = form
+    @attribute = attribute
+    @collection = collection
+    @options = options
+  end
+
+  def view_template
+    Components::Form::Select(@form, @attribute, collection: @collection, **@options)
+  end
+end
