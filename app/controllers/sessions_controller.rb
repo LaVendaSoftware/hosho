9d55d56c79c9211173a8014c75fcc55ceee58298
Walmir <owalmirneto@gmail.com>
2025-09-17 09:39:28 -0300

    Fix login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
     with: -> { redirect_to new_session_url, alert: t(".try_again_later") }
 
   def new
-    render Views::Sessions::New.new
+    @user = User.new
   end
 
   def create
-    user = User.where(disabled_at: nil).authenticate_by(params.permit(:email_address, :password))
+    user = User.where(disabled_at: nil).authenticate_by(session_params)
 
     if user
       start_new_session_for(user)
@@ -23,5 +23,11 @@ class SessionsController < ApplicationController
   def destroy
     terminate_session
     redirect_to new_session_path
+  end
+
+  private
+
+  def session_params
+    params.expect(user: [:email_address, :password])
   end
 end
