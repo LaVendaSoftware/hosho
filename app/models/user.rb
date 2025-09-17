a27c6f1f972eafa30dfebcd5148e54feb37c2521
Walmir <owalmirneto@gmail.com>
2025-09-17 16:05:12 -0300

    Implement user's CRUD

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   include PIDable
+  include Humanizable
 
   has_secure_password
   has_many :sessions, dependent: :destroy
@@ -9,6 +10,14 @@ class User < ApplicationRecord
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   enum :role, {standard: 0, seller: 1, manager: 2, admin: 99, developer: 999}
+
+  validates :name, presence: true
+  validates :email_address, presence: true
+  validates :role, inclusion: {in: User.roles.keys}
+
+  def humanized_role
+    human_enum_singular_name(:role)
+  end
 
   def disabled? = disabled_at.present?
   alias_method :disabled, :disabled?
