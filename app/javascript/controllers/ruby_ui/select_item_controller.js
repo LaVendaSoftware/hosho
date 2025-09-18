8c97ca21906ddef58909780ac489a8719d8c04e3
Walmir <owalmirneto@gmail.com>
2025-09-18 10:46:31 -0300

    Fix Select component js

--- app/javascript/controllers/ruby_ui/select_item_controller.js
+++ app/javascript/controllers/ruby_ui/select_item_controller.js
@@ -1,8 +1,8 @@
 import { Controller } from "@hotwired/stimulus";
-export default class extends Controller {
 
+export default class extends Controller {
   handleSelectItem({ target }) {
-    if (this.element.dataset.value == target.dataset.value) {
+    if (this.element.dataset.value === target.dataset.value) {
       this.element.setAttribute("aria-selected", true);
     } else {
       this.element.removeAttribute("aria-selected");
