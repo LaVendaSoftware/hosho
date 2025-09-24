30a5d67afeee5e8fc250e59c580f3a42ed89a1e0
Walmir <owalmirneto@gmail.com>
2025-09-23 22:32:20 -0300

    Add RubyUI Command component

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,22 +2,28 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
 
-import RubyUi__CheckboxGroupController from "./ruby_ui/checkbox_group_controller"
-application.register("ruby-ui--checkbox-group", RubyUi__CheckboxGroupController)
+import RubyUi__CheckboxGroupController from "./ruby_ui/checkbox_group_controller";
+application.register(
+  "ruby-ui--checkbox-group",
+  RubyUi__CheckboxGroupController,
+);
 
-import RubyUi__ComboboxController from "./ruby_ui/combobox_controller"
-application.register("ruby-ui--combobox", RubyUi__ComboboxController)
+import RubyUi__ComboboxController from "./ruby_ui/combobox_controller";
+application.register("ruby-ui--combobox", RubyUi__ComboboxController);
 
-import RubyUi__FormFieldController from "./ruby_ui/form_field_controller"
-application.register("ruby-ui--form-field", RubyUi__FormFieldController)
+import RubyUi__CommandController from "./ruby_ui/command_controller";
+application.register("ruby-ui--command", RubyUi__CommandController);
 
-import RubyUi__SelectController from "./ruby_ui/select_controller"
-application.register("ruby-ui--select", RubyUi__SelectController)
+import RubyUi__FormFieldController from "./ruby_ui/form_field_controller";
+application.register("ruby-ui--form-field", RubyUi__FormFieldController);
 
-import RubyUi__SelectItemController from "./ruby_ui/select_item_controller"
-application.register("ruby-ui--select-item", RubyUi__SelectItemController)
+import RubyUi__SelectController from "./ruby_ui/select_controller";
+application.register("ruby-ui--select", RubyUi__SelectController);
+
+import RubyUi__SelectItemController from "./ruby_ui/select_item_controller";
+application.register("ruby-ui--select-item", RubyUi__SelectItemController);
