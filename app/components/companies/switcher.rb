40cadb929620e696bac566ad107054821a64df0d
Walmir <owalmirneto@gmail.com>
2025-09-23 22:32:58 -0300

    Use Command component to switch company

--- app/components/companies/switcher.rb
+++ app/components/companies/switcher.rb
@@ -7,27 +7,33 @@ class Components::Companies::Switcher < Components::Base
   def view_template
     return if @companies.blank?
 
-    div(class: "my-4") do
-      select(class: select_classes) do
-        @companies.each do |company|
-          option(value: company.pid, selected: @current_company_pid == company.pid) { company.name }
+    CommandDialog do
+      CommandDialogTrigger do
+        Button(variant: "outline", class: "w-56 pr-2 pl-3 justify-between") do
+          div(class: "flex items-center space-x-1") do
+            Components::Icon("magnifying-glass")
+            span(class: "text-muted-foreground font-normal") do
+              t(".change")
+            end
+          end
+        end
+      end
+      CommandDialogContent do
+        Command do
+          CommandInput(placeholder: t(".search_placeholder"))
+          CommandEmpty { t(".not_found") }
+          CommandList do
+            CommandGroup(title: tmp(Company)) do
+              @companies.each do |company|
+                CommandItem(value: company[:name], href: company_path(company)) do
+                  Components::Icon("arrows-left-right")
+                  plain company[:name]
+                end
+              end
+            end
+          end
         end
       end
     end
-  end
-
-  private
-
-  def select_classes
-    [
-      "file:text-foreground placeholder:text-muted-foreground selection:bg-primary ",
-      "selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full ",
-      "min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs ",
-      "transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 ",
-      "file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none ",
-      "disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:border-ring ",
-      "focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 ",
-      "dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
-    ]
   end
 end
