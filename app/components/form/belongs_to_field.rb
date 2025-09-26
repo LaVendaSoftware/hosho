d2e4db982e65bb203961c9d0afe4662ebe8c7805
Walmir <owalmirneto@gmail.com>
2025-09-26 01:00:14 -0300

    Fix BelongsToField collection

--- app/components/form/belongs_to_field.rb
+++ app/components/form/belongs_to_field.rb
@@ -1,7 +1,7 @@
 class Components::Form::BelongsToField < Components::Form::Select
   def initialize(form, association, collection: [], **options)
     @association = association
-    super(form, "#{association}_id", collection: [], **options)
+    super(form, "#{association}_id", collection:, **options)
   end
 
   def errors
