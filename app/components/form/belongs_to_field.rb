c5603fa5d19fb0f0e429698393f0819005fe44c6
Walmir <owalmirneto@gmail.com>
2025-09-26 00:37:16 -0300

    Fix BelongsToField validation

--- app/components/form/belongs_to_field.rb
+++ app/components/form/belongs_to_field.rb
@@ -1,12 +1,17 @@
-class Components::Form::BelongsToField < Components::Base
-  def initialize(form, attribute, collection: [], **options)
-    @form = form
-    @attribute = attribute
-    @collection = collection
-    @options = options
+class Components::Form::BelongsToField < Components::Form::Select
+  def initialize(form, association, collection: [], **options)
+    @association = association
+    super(form, "#{association}_id", collection: [], **options)
   end
 
-  def view_template
-    Components::Form::Select(@form, "#{@attribute}_id", collection: @collection, **@options)
+  def errors
+    return if full_messages.blank?
+
+    span(class: "text-red-500 italic") { full_messages.to_sentence }
+  end
+
+  def full_messages
+    @form.object.errors.full_messages_for(@association).presence ||
+      @form.object.errors.full_messages_for(@attribute)
   end
 end
