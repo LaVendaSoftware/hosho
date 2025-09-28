b445c06dabf21339cefaccce8bcea35a2370a05e
Walmir <owalmirneto@gmail.com>
2025-09-28 11:45:59 -0300

    Fix order model and create payments models

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,12 +8,14 @@ class Order < ApplicationRecord
 
   has_many :items, class_name: "OrderItem", dependent: :restrict_with_error
   has_many :product_variants, through: :items
+  has_many :payments, dependent: :restrict_with_error
+  has_many :payment_attempts, through: :payments
 
   delegate :name, :email_address, to: :customer, allow_nil: true, prefix: true
 
   accepts_nested_attributes_for :items, allow_destroy: true
 
-  enum :status, {pending: 0, ready: 1}
+  enum :status, {draft: 0, ready: 1, partially_paid: 2, paid: 3, cancelled: 4}
 
   validates :items, length: {minimum: 1}
 
