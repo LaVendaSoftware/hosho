be2a0e890c924b4c5500483a8f779cc88cf7409a
Walmir <owalmirneto@gmail.com>
2025-10-22 15:41:54 -0300

    Fix fetch product prices

--- app/javascript/controllers/order_items_controller.js
+++ app/javascript/controllers/order_items_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="product-variants"
+// Connects to data-controller="order-items"
 export default class extends Controller {
   static targets = ["productVariantId", "unitPrice", "quantity", "subtotal"];
 
